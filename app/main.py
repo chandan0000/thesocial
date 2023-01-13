5b265980ca3b780b0a948d2df4e58d95591a605b
chandan kumar <kumarchandan41u@gmail.com>
2023-01-13 23:18:43 +0530

    auth

--- app/main.py
+++ app/main.py
@@ -3,17 +3,17 @@ from fastapi import FastAPI, Response, status, HTTPException, Depends
 from sqlalchemy.orm import Session
 from . import models,schemas,utils
 from .databses import engine,get_db
-from .router import post,user
+from .router import post,user,auth
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
 
-app.include_router(post.router)
 
 app.include_router(post.router)
 app.include_router(user.router)
+app.include_router(auth.router)
 
 @app.get("/",tags=['Root'])
 def root():
--- app/schemas.py
+++ app/schemas.py
@@ -36,3 +36,6 @@ class UserOut(BaseModel):
 
     class Config:
         orm_mode = True
+class UserLogin(BaseModel):
+    email:EmailStr
+    password:str
